97231d0a642c2ae4a5856d3193ab4a5a957b0890
bitbytebit <tribe321@gmail.com>
2020-04-07 03:31:02 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home/Container.vue'
 import Matrix from './views/Matrix.vue'
+import Deepspacebyte from './views/Deepspacebyte.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/matrix',
       name: 'matrix',
       component: Matrix
+    },
+    {
+      path: '/deepspacebyte',
+      name: 'deepspacebyte',
+      component: Deepspacebyte
     }
   ]
 })
