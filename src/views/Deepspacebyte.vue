c47a34073c9530918c19fabb00ac97d1b1ff60c0
bitbytebit <tribe321@gmail.com>
2020-04-07 03:31:00 +0200

    Create Deepspacebyte.vue

---
+++ src/views/Deepspacebyte.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-content>
+      <div>wasd</div>
+    </v-content>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
